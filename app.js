f6a9344ab6a653405838a7244c4ef4d3ee6c525e
Vinod Jhon <vinod.designer1@gmail.com>
2014-10-20 03:14:27 +0530

    changed port

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ routes.configRoutes( app, server );
 // -------------- END SERVER CONFIGURATION ----------------
 
 // ----------------- BEGIN START SERVER -------------------
-server.listen( 4000 );
+server.listen( 8080 );
 console.log(
   'Express server listening on port %d in %s mode',
    server.address().port, app.settings.env
